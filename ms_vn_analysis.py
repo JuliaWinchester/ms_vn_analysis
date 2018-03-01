5a36bb0646de1295e29307b2b4337a1370672c02
Julie Winchester <julie.winchester@duke.edu>
2018-02-28 22:46:52 -0500

    Fleshed out ms_vn_analysis.py

--- ms_vn_analysis.py
+++ ms_vn_analysis.py
@@ -3,12 +3,14 @@
 # Author: Julie Winchester <julia.m.winchester@gmail.com>
 # February 14, 2018
 
+from requests import Request, Session
 import credentials
 import pymysql
 import requests
+import cPickle as pickle
 
 def db_conn():
-	return pymysql.connect(host = credentials.db['server'],
+	return pymysql.connect(unix_socket = credentials.db['socket'],
 						   user = credentials.db['username'],
 						   password = credentials.db['password'],
 						   db = credentials.db['db'],
@@ -21,6 +23,9 @@ def db_query(cursor, sql, args=None):
 	cursor.execute(sql, args)
 	return cursor.fetchall()
 
+def vn_url(genus, species):
+	return 'http://api.vertnet-portal.appspot.com/api/search?q=%7B%22q%22:%22genus:' + genus + '%20specificepithet:' + species + '%22%7D' 
+
 conn = db_conn()
 c = conn.cursor()
 
@@ -28,7 +33,56 @@ sql = """ SELECT DISTINCT genus, species FROM ms_taxonomy_names """
 
 r = db_query(c, sql)
 
+genusDict = {}
+for t in r:
+	print t['genus'] + ' ' + t['species']
+	if t['genus'] not in genusDict:
+		genusDict[t['genus']] = {}
+		genusDict[t['genus']]['sp'] = []
+		genusDict[t['genus']]['taxonomies'] = []
+	genusDict[t['genus']]['sp'].append(t['species'])
+	resp = requests.get(vn_url(t['genus'], t['species']))
+	json = resp.json()
+	for specimen in json['recs']:
+		if 'kingdom' in specimen:
+			k = specimen['kingdom']
+		else:
+			k = ''
+		if 'phylum' in specimen:
+			p = specimen['phylum']
+		else:
+			p = ''
+		if 'class' in specimen:
+			c = specimen['class']
+		else:
+			c = ''
+		if 'order' in specimen:
+			o = specimen['order']
+		else:
+			o = ''
+		if 'family' in specimen:
+			f = specimen['family']
+		else:
+			f = ''
+		if 'genus' in specimen:
+			g = specimen['genus']
+		else:
+			g = ''
+		if 'specificepithet' in specimen:
+			s = specimen['specificepithet']
+		else:
+			s = ''
+		genusDict[t['genus']]['taxonomies'].append({
+			'kingdom' : k,
+			'phylum' : p,
+			'class' : c,
+			'order' : o,
+			'family' : f,
+			'genus' : g,
+			'species' : s 
+		})
 
+pickle.dump(genusDict, open("genusDict.p", "wb"))
 
 
 
